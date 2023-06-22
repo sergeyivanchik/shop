f0081d03694a75c995b0dc0b36273bb133c16dfb
Sergey Ivanchik <ivanchik-s@mail.ru>
2023-06-22 23:34:17 +0300

    add top block for product page

--- src/modules/product-card/product-card.styles.ts
+++ src/modules/product-card/product-card.styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { colors } from '@/constants';
+
 import { Container } from '@/components';
 
 const ProductCardStyled = styled(Container)`
@@ -12,4 +14,80 @@ const ProductCardStyled = styled(Container)`
   }
 `;
 
-export { ProductCardStyled };
+const InfoStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  max-width: 386px;
+  width: 100%;
+  height: 380px;
+`;
+
+const WrapperStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TitleStyled = styled.span`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  width: 100%;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  font-weight: 500;
+  font-size: 18px;
+  color: ${colors.white};
+`;
+
+const PriceStyled = styled.span`
+  margin-top: 10px;
+  color: ${colors.gray100};
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const DescriptionStyled = styled.span`
+  color: ${colors.gray200};
+  text-overflow: ellipsis;
+  overflow: hidden;
+  width: 100%;
+  display: -webkit-box;
+  -webkit-line-clamp: 7;
+  -webkit-box-orient: vertical;
+  margin-top: 15px;
+`;
+
+const FooterStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ButtonsContainerStyled = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+
+  & > *:not(:last-child) {
+    margin-right: 10px;
+  }
+`;
+
+const PurchasedStyled = styled.span`
+  color: ${colors.gray700};
+  font-size: 10px;
+  font-weight: 500;
+`;
+
+export {
+  ProductCardStyled,
+  InfoStyled,
+  TitleStyled,
+  PriceStyled,
+  DescriptionStyled,
+  WrapperStyled,
+  FooterStyled,
+  ButtonsContainerStyled,
+  PurchasedStyled,
+};
--- src/modules/product-card/product-card.tsx
+++ src/modules/product-card/product-card.tsx
@@ -1,14 +1,23 @@
-import { FC } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useGetProductQuery } from '@/store';
 
-import { Loading } from '@/components';
+import { Button, Loading } from '@/components';
 import { Preview } from './components';
 
-import { ProductCardStyled } from './product-card.styles';
+import {
+  ProductCardStyled,
+  InfoStyled,
+  TitleStyled,
+  PriceStyled,
+  DescriptionStyled,
+  WrapperStyled,
+  FooterStyled,
+  ButtonsContainerStyled,
+  PurchasedStyled,
+} from './product-card.styles';
 
-const ProductCard: FC = () => {
+const ProductCard = () => {
   const { productId = '' } = useParams();
   const { data: product, isLoading } = useGetProductQuery(productId);
 
@@ -16,6 +25,28 @@ const ProductCard: FC = () => {
   const hasProduct = !isLoading && !!product && (
     <>
       <Preview images={product?.images || []} />
+      <InfoStyled>
+        <WrapperStyled>
+          <TitleStyled>{product.title}</TitleStyled>
+          <PriceStyled>{product.price}$</PriceStyled>
+          <DescriptionStyled>{product.description}</DescriptionStyled>
+          <ButtonsContainerStyled>
+            <Button
+              title="Add to cart"
+              onclick={() => console.log('Add to cart')}
+            />
+            <Button
+              title="Add to favorites"
+              onclick={() => console.log('Add to favorites')}
+            />
+          </ButtonsContainerStyled>
+        </WrapperStyled>
+        <FooterStyled>
+          <PurchasedStyled>
+            {Math.floor(Math.random() * 100)} people purchased
+          </PurchasedStyled>
+        </FooterStyled>
+      </InfoStyled>
     </>
   );
 
